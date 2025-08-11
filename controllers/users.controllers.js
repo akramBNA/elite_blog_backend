f84f5b0187fc8f28a9fa14ba466b82e2f36b2cd1
Akram <akrambenaoun1993@gmail.com>
2025-08-11 21:37:51 +0100

    create a signup controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -5,4 +5,5 @@ module.exports = {
   addUser: (req, res, next) => usersInstance.addUser(req, res, next),
   getAllActiveUsers: (req, res, next) => usersInstance.getAllActiveUsers(req, res, next),
   getUserById: (req, res, next) => usersInstance.getUserById(req, res, next),
+  SignUp: (req, res, next) => usersInstance.SignUp(req, res, next),
 };
