9329ef1f6e3c9a41498864edaa076c8c25f8c343
Akram <akrambenaoun1993@gmail.com>
2025-08-11 21:13:28 +0100

    create a getAllActiveUsrers() controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,4 +3,5 @@ const usersInstance = new UsersDao();
 
 module.exports = {
   addUser: (req, res, next) => usersInstance.addUser(req, res, next),
+  getAllActiveUsers: (req, res, next) => usersInstance.getAllActiveUsers(req, res, next),
 };
