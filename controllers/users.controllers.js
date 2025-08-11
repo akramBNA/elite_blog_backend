01eee5bd70a0c336eb864cfd29b4e4d1ab82a178
Akram <akrambenaoun1993@gmail.com>
2025-08-11 21:25:13 +0100

    create a getUserById() controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -4,4 +4,5 @@ const usersInstance = new UsersDao();
 module.exports = {
   addUser: (req, res, next) => usersInstance.addUser(req, res, next),
   getAllActiveUsers: (req, res, next) => usersInstance.getAllActiveUsers(req, res, next),
+  getUserById: (req, res, next) => usersInstance.getUserById(req, res, next),
 };
