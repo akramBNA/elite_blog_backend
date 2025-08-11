54decfa26cba53323498d2197a40a5a89f86e137
Akram <akrambenaoun1993@gmail.com>
2025-08-11 22:03:06 +0100

    add login controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -6,4 +6,5 @@ module.exports = {
   getAllActiveUsers: (req, res, next) => usersInstance.getAllActiveUsers(req, res, next),
   getUserById: (req, res, next) => usersInstance.getUserById(req, res, next),
   SignUp: (req, res, next) => usersInstance.SignUp(req, res, next),
+  login: (req, res, next) => usersInstance.login(req, res, next),
 };
