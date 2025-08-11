7cc68533fcb23e9087813c06f2d0baebd617cf22
Akram <akrambenaoun1993@gmail.com>
2025-08-11 19:48:01 +0100

    create roles schema

--- models/roles.model.js
+++ models/roles.model.js
@@ -1,8 +1,8 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const roleSchema = new mongoose.Schema({
-//   roleType: { type: String, required: true, unique: true, trim: true },
-//   active: { type: Boolean, default: true }
-// }, { timestamps: true });
+const roleSchema = new mongoose.Schema({
+  roleType: { type: String, required: true, unique: true, trim: true },
+  active: { type: Boolean, default: true }
+}, { timestamps: true });
 
-// module.exports = mongoose.model('Role', roleSchema);
+module.exports = mongoose.model('Role', roleSchema);
