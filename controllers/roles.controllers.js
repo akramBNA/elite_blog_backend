12bc7b6078c1974d4941761420dab4d3f4ab31b2
Akram <akrambenaoun1993@gmail.com>
2025-08-12 18:03:56 +0100

    create update role controller

--- controllers/roles.controllers.js
+++ controllers/roles.controllers.js
@@ -4,4 +4,5 @@ const roles_instance = new RolesDao();
 module.exports = {
   getAllRoles: (req, res, next) => roles_instance.getAllRoles(req, res, next),
   addRole: (req, res, next) => roles_instance.addRole(req, res, next),
+  updateRole: (req, res, next) => roles_instance.updateRole(req, res, next),
 };
