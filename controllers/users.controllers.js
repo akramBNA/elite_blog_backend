1d64e0ee00461d92fc42bf25371ee7eb1ce5c930
Akram <akrambenaoun1993@gmail.com>
2025-08-12 13:36:43 +0100

    create a users stats controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -4,6 +4,7 @@ const usersInstance = new UsersDao();
 module.exports = {
   addUser: (req, res, next) => usersInstance.addUser(req, res, next),
   getAllActiveUsers: (req, res, next) => usersInstance.getAllActiveUsers(req, res, next),
+  getUserStats: (req, res, next) => usersInstance.getUserStats(req, res, next),
   getUserById: (req, res, next) => usersInstance.getUserById(req, res, next),
   SignUp: (req, res, next) => usersInstance.SignUp(req, res, next),
   login: (req, res, next) => usersInstance.login(req, res, next),
