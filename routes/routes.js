85d9d2ea37f01c5fb39b4d370dcdd42d8cc7d044
Akram <akrambenaoun1993@gmail.com>
2025-08-13 14:24:53 +0100

    add backend permissions + role middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middlewares/authentication.middlewares');
+const authorizeRole = require("../middlewares/authorizeRole");
 const rolesControllers = require('../controllers/roles.controllers');
 const usersControllers = require('../controllers/users.controllers');
 const postsControllers = require('../controllers/posts.controllers');
@@ -8,15 +9,15 @@ const commentsControllers = require('../controllers/comments.controllers');
 
 
 // ROLES ROUTES
-router.get('/roles/getAllRoles', authenticateToken, rolesControllers.getAllRoles);
-router.post('/roles/addRole', authenticateToken, rolesControllers.addRole);
-router.put('/roles/updateRole/:id', authenticateToken, rolesControllers.updateRole)
+router.get('/roles/getAllRoles', authenticateToken, authorizeRole('Admin'), rolesControllers.getAllRoles);
+router.post('/roles/addRole', authenticateToken, authorizeRole('Admin'), rolesControllers.addRole);
+router.put('/roles/updateRole/:id', authenticateToken, authorizeRole('Admin'), rolesControllers.updateRole)
 
 // USERS ROUTES
 router.post('/users/addUser', usersControllers.addUser);
-router.get('/users/getAllActiveUsers', authenticateToken, usersControllers.getAllActiveUsers);
-router.get('/users/getUserStats', authenticateToken, usersControllers.getUserStats);
-router.get('/users/getUserById/:params', authenticateToken, usersControllers.getUserById);
+router.get('/users/getAllActiveUsers', authenticateToken, authorizeRole('Admin'), usersControllers.getAllActiveUsers);
+router.get('/users/getUserStats', authenticateToken, authorizeRole('Admin'), usersControllers.getUserStats);
+router.get('/users/getUserById/:params', authenticateToken, authorizeRole('Admin'), usersControllers.getUserById);
 router.post('/users/signUp', usersControllers.SignUp);
 router.post('/users/login', usersControllers.login);
 router.post('/users/refreshToken', usersControllers.refreshToken);
@@ -25,8 +26,8 @@ router.post('/users/logout', authenticateToken, usersControllers.logout);
 // POSTS ROUTES
 router.post('/posts/createPost', authenticateToken, postsControllers.createPost);
 router.get('/posts/getAllPosts', authenticateToken, postsControllers.getAllPosts);
-router.put('/posts/deletePost/:id', authenticateToken, postsControllers.deletePost);
-router.put('/posts/updatePost/:id', authenticateToken, postsControllers.updatePost);
+router.put('/posts/deletePost/:id', authenticateToken, authorizeRole('Admin'), postsControllers.deletePost);
+router.put('/posts/updatePost/:id', authenticateToken, authorizeRole('Admin', 'Editor', 'Writer'), postsControllers.updatePost);
 
 
 // COMMENTS ROUTES
