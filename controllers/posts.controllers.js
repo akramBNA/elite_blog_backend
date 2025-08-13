8d539b05e8e05956e3c2e021e1274b18aa901eac
Akram <akrambenaoun1993@gmail.com>
2025-08-13 11:38:43 +0100

    create an update post controller

--- controllers/posts.controllers.js
+++ controllers/posts.controllers.js
@@ -5,4 +5,5 @@ module.exports = {
   createPost: (req, res, next) => posts_instance.createPost(req, res, next),
   getAllPosts: (req, res, next) => posts_instance.getAllPosts(req, res, next),
   deletePost: (req, res, next) => posts_instance.deletePost(req, res, next),
+  updatePost: (req, res, next) => posts_instance.updatePost(req, res, next),
 };
